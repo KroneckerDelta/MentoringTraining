aedb44815dd7bd5d375f64b8b65a9a291d6efe81
Thomas Michael <thomas.michael@anmi24.de>
2013-05-08 22:14:26 +0200

    adding controller class

---
+++ src/de/mic/mentoring/personsample/Controller.java
@@ -0,0 +1,19 @@
+package de.mic.mentoring.personsample;
+
+public class Controller {
+
+	PersonDao dao;
+
+	public Controller(PersonDao dao) {
+		this.dao = dao;
+	}
+
+	public void showPersonList() {
+
+	}
+
+	public void actionShowDetail() {
+
+	}
+
+}
